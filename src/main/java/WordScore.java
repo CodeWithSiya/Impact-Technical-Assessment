714e6244bd9277c6a186ef1b89c8fe37c86dbee8
Siyabonga Madondo <mdnsiy014@myuct.ac.za>
2024-09-23 21:09:21 +0000

    Updated WordScore.java - Added Word and Letter Multiplier Extension.

--- src/main/java/WordScore.java
+++ src/main/java/WordScore.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * The WordScore class computes the Scrabble score for a given word based on the Scrabble point system.
  * This class provides functionality to calculate and obtain the total score of a given word.
  * 
- * It assigns values to letters according to the following system:
+ * It assigns values to letters according to the following system.
  * Letter                           Value
  * A, E, I, O, U, L, N, R, S, T       1
  * D, G                               2
@@ -16,7 +16,7 @@ import java.util.ArrayList;
  * J, X                               8
  * Q, Z                               10
  * 
- * Extentions: Allow the user to specify double or triple letters/words.
+ * Extensions: Allow the user to specify letter and word multipliers.
  * 
  * @author Siyabonga Madondo
  * @version 23/09/2024
@@ -25,7 +25,7 @@ import java.util.ArrayList;
 public class WordScore 
 {
     /** 
-     * A HashMap that stores the Scrabble point values for different groups of letters.
+     * A HashMap that stores the Scrabble point values for different letter categories.
      * The keys are arrays of letters, and the values represent the point value of those letters.
      */
     private static HashMap<ArrayList<String>, Integer> points;
@@ -36,33 +36,29 @@ public class WordScore
     private int score;
 
     /**
-     * Constants for the double and triple letter multipliers.
-     */
-    private static final int DOUBLE_LETTER = 2;
-    private static final int TRIPLE_LETTER = 3;
- 
-    /**
-     * Constants for the double and triple word multipliers.
-     */
-    private static final int DOUBLE_WORD = 2;
-    private static final int TRIPLE_WORD = 3;
-
-    /**
-     * Create a new WordScore object from the given word and compute it's score.
+     * Create a new WordScore object from the given word and compute its score.
      * @param word The word which the Scrabble score needs to be computed for.
      */
     public WordScore(String word) {
-        // Initialise the point system and the score for the current word.
         WordScore.initialisePointSystem();
-        // Compute the score for the current word and set the score.
         this.score = WordScore.computeScore(word);
+    }
+
+    /**
+     * Create a new WordScore object from the given word and compute its score with multipliers.
+     * @param word The word which the Scrabble score needs to be computed for.
+     * @param letterMultipliers An array of integers representing multipliers for each letter.
+     * @param wordMultiplier The multiplier for the entire word score.
+     */
+    public WordScore(String word, int[] letterMultipliers, int[] wordMultiplier) {
+        WordScore.initialisePointSystem();
+        this.score = WordScore.computeScore(word, letterMultipliers, wordMultiplier);
     }
 
     /**
      * Initialises the letter categories and their corresponding Scrabble point values.
      */
     private static void initialisePointSystem() {
-        // Initialise the points HashMap.
         points = new HashMap<>();
         // Add each letter group and their corresponding point values to the points HashMap.
         points.put(new ArrayList<>(Arrays.asList("A", "E", "I", "O", "U", "L", "N", "R", "S", "T")), 1);
@@ -75,7 +71,7 @@ public class WordScore
     }
 
     /**
-     * Utility method which computes the Scrabble score for the given word.
+     * Computes the Scrabble score for the given word.
      * @param word The word which the Scrabble score needs to be computed for.
      * @return The total score for the given word.
      */
@@ -84,7 +80,7 @@ public class WordScore
         int score = 0;
         // Loop through each character of the word to obtain the score.
         for (int i = 0; i < word.length(); i++) {
-            // Obtain the current letter from the word.
+            // Obtain the current letter from the word and convert it to uppercase.
             String letter = word.substring(i, i+1).toUpperCase();
             // Loop through each category in the points HashMap.
             for (ArrayList<String> category : points.keySet()) {
@@ -98,7 +94,39 @@ public class WordScore
         }
         return score;
     }
-    
+
+    /**
+     * Computes the Scrabble score for the given word with letter and word multipliers.
+     * @param word The word for which the Scrabble score needs to be computed.
+     * @param letterMultipliers An array of integers representing multipliers for each letter.
+     * @param wordMultiplier The multiplier for the entire word score.
+     * @return The total score for the given word after applying multipliers.
+     * @throws IllegalArgumentException if the length of letterMultipliers doesn't match the word length
+     */
+    private static int computeScore(String word, int[] letterMultipliers, int wordMultiplier) throws IllegalArgumentException {
+        // Check if the length of letterMultipliers matches the word length.
+        if (word.length() != letterMultipliers.length) {
+            throw IllegalArgumentException("Length of letterMultipliers must match the word length");
+        } 
+        // Initialise the score to zero.
+        int score = 0;
+        // Loop through each character of the word to obtain the score.
+        for (int i = 0; i < word.length(); i++) {
+            // Obtain the current letter from the word.
+            String letter = word.substring(i, i+1).toUpperCase();
+            // Loop through each category in the points HashMap.
+            for (ArrayList<String> category : points.keySet()) {
+                // Check if the current category contains the current letter.
+                if (category.contains(letter)) {
+                    // Add the corresponding multiplied point value and exit the inner loop.
+                    score += points.get(category) * letterMultipliers[i];
+                    break; 
+                }
+            }
+        }
+        return score * wordMultiplier;
+    }
+
     /**
      * Obtain the score for the current word.
      * @return  The total Scrabble score for the current word.
