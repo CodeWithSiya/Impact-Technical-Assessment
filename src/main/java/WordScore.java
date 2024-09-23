d35bc9b4b0c2c604308f666dc7bfc99085a7a772
Siyabonga Madondo <mdnsiy014@myuct.ac.za>
2024-09-23 18:49:24 +0000

    Updated WordScore.java - Implemented the initialisePointSystem method to initialise the points HashMap.

--- src/main/java/WordScore.java
+++ src/main/java/WordScore.java
@@ -23,12 +23,26 @@ public class WordScore
      * A HashMap that stores the Scrabble point values for different groups of letters.
      * The keys are arrays of letters, and the values represent the point value of those letters.
      */
-    private HashMap<String[], Integer> points;
+    private static HashMap<String[], Integer> points;
 
     /** 
      * Stores the total score for the word being processed. 
      */
     private int score;
+
+    /**
+     * Initialises the letter groups and their corresponding Scrabble point values.
+     */
+    private void initialisePointSystem() {
+        // Add each letter group and their corresponding point values to the points HashMap.
+        points.put(new String[]{"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"}, 1);
+        points.put(new String[]{"D", "G"}, 2);
+        points.put(new String[]{"B", "C", "M", "P"}, 3);
+        points.put(new String[]{"F", "H", "V", "W", "Y"}, 4);
+        points.put(new String[]{"K"}, 5);
+        points.put(new String[]{"J", "X"}, 8);
+        points.put(new String[]{"Q", "Z"}, 10);
+    }
 
     WordScore(String word) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
@@ -37,5 +51,4 @@ public class WordScore
     int getScore() {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
-
 }
