3e02d8e31edb36e04c02c9f890730077a09f4855
Siyabonga Madondo <mdnsiy014@myuct.ac.za>
2024-09-23 19:23:50 +0000

    Updated WordScore.java - Implemented the computeScore utility method to compute the Scrabble score of a given word.

--- src/main/java/WordScore.java
+++ src/main/java/WordScore.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
  * @author Siyabonga Madondo
  * @version 23 September 2024
  */
-
+@SuppressWarnings("unused")
 public class WordScore 
 {
     /** 
@@ -46,6 +46,29 @@ public class WordScore
         points.put(new ArrayList<>(Arrays.asList("Q", "Z")), 10);
     }
 
+    /**
+     * Utility method which computes the Scrabble score for the given word.
+     * @param word The word which the Scrabble score needs to be computed for.
+     * @return The total score for the given word.
+     */
+    private int computeScore(String word) {
+        // Initialise the score to zero.
+        int score = 0;
+        // Loop through each character of the word to obtain the score.
+        for (int i = 0; i < word.length(); i++) {
+            // Obtain the current letter from the word.
+            String letter = word.substring(i, i+1);
+            // Loop through each category in the points HashMap.
+            for (ArrayList<String> category : points.keySet()) {
+                // Check if the current category contains the current letter.
+                if (category.contains(letter)) {
+                    score += points.get(category);  // Add the corresponding point value.
+                }
+            }
+        }
+        return score;
+    }
+    
     WordScore(String word) {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
